303fb7f9cf338f37675838cb5a8ae62376ba5a1e
OLOZ4 <36029235+OLOZ4@users.noreply.github.com>
2024-09-29 19:36:52 +0300

    Added mysql support
    also added mysqli to php

---
+++ pagrindinis/test.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "mysql";
+$username = "test";
+$password = "test";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?> 
