528daba8fb274e681e024dc590c65f173c173c61
OLOZ4 <36029235+OLOZ4@users.noreply.github.com>
2024-10-06 17:21:05 +0300

    minor changes
    nginx kazkodel neveikia jei failo pavadinime yra brukniukas, todel pervadinau

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -7,10 +7,14 @@ CREATE TABLE IF NOT EXISTS users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255),
   email VARCHAR(255),
-  password VARCHAR(255)
+  password VARCHAR(255),
+  UNIQUE (name, email)
 );
 
 -- Insert some initial data
-INSERT INTO users (name, email, password) VALUES
-  ('John Doe', 'john@example.com', 'password123'),
-  ('Benas Tenas', 'jane@example.com', 'password123');
+INSERT INTO users (name, email, password) VALUES 
+('Testas', 'johndoe@example.com', 'pass123')
+ON DUPLICATE KEY UPDATE
+    name = VALUES(name),
+    email = VALUES(email),
+    password = VALUES(password);
