df0e1c2aa13ed50f1df1e96e017f424616a0cef5
OLOZ4 <36029235+OLOZ4@users.noreply.github.com>
2024-10-14 22:14:15 +0300

    added comments to signup.php

--- pagrindinis/signup.php
+++ pagrindinis/signup.php
@@ -1,47 +1,31 @@
 <?php
-require_once("config.php");
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+require_once("config.php"); // Sitame faile yra saugomi prisijungimo prie db infomacija
 
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $confirm_password = $_POST['confirm-password'];
-    $email = $_POST['email'];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') { // Jeigu html kode yra naudojamas post metodas
+
+    $username = $_POST['username']; // Vartojo ivestas username prilyginamas kintamajam 'username'
+    $password = $_POST['password']; // Vartojo ivestas password prilyginamas kintamajam 'password'
+    $confirm_password = $_POST['confirm-password']; // Vartojo ivestas pakartotinis slaptazodis prilyginamas kintamajam 'confirm_password'
+    $email = $_POST['email']; // Vartojo ivestas email prilyginamas kintamajam 'email'
 
 
-    if($password == $confirm_password)
+    if($password == $confirm_password) // Jeigu abu vartotojo ivesti slaptazodziai sutampa tada:
         {
-            $password_hash = password_hash($password,PASSWORD_BCRYPT);
-            $query = "SELECT * FROM users WHERE username='$username'";
-            $result = $conn->query($query);
+            $password_hash = password_hash($password,PASSWORD_BCRYPT); // Uzheshuojamas ivestas slaptazodis naudojant BCRYPT ir prilyginamas 'password_hash' kintamajam
+           
+            $result = $conn->query("SELECT * FROM users WHERE username='$username'"); // Patikrinama ar yra jau egsistuojantis username
 
-            $query1 = "SELECT * FROM users WHERE email='$email'";
-            $result1 = $conn->query($query1);
+            $result1 = $conn->query("SELECT * FROM users WHERE email='$email'"); // Patikrinama ar yra jau egsistuojantis email
 
-            if($result->num_rows == 0 AND $result1->num_rows == 0)
+            if($result->num_rows == 0 AND $result1->num_rows == 0) // Jei toks username ir email NEegzistuoja tada:
                 {
-                    $query = "INSERT INTO users (username,email,password) VALUES ('$username', '$email', '$password_hash')";
-                    $result = $conn->query($query);
-                    
-                    if ($result)
-                    {
-                        echo 'susikurti paskyra pavyko!';
-                    }
-                    else 
-                    {
-                        echo 'Klaida: ' . $conn->error_get_last;
-                    }
+                    $result = $conn->query("INSERT INTO users (username,email,password) VALUES ('$username', '$email', '$password_hash')"); // I db username email ir passwords irasyti vartotojo ivestus duomenis 
+                    if ($result) echo 'Account creation was successful!'; // Jei  pavyko insertinti duomenis i db pranesti apie tai vartotojui
+                        else echo 'Error: ' . $conn->error_get_last; // Kitaip ismesti klaida
                 }
-            else
-                {
-                    echo '<h1>Username or E-mail already exists</h1><br><br>';
-                    //echo 'Klaida: ' . $conn->error_get_last;
-
-                }
+            else echo '<h1>Username or E-mail already exists</h1></br></br>'; // Jei email ar username sutampa, apie tai pranesti vartotojui
         }
-    else
-        {
-            echo '<h1>Passwords do not match!</h1>';
-        }
+    else echo '<h1>Passwords do not match!</h1>'; // Pranesti kad slaptazodziai nesutampa
 }   
 ?>
 
