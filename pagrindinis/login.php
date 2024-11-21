8e2be24f8d7e9d0029b3ec28f3bf3bebf16b0bf6
frongerio <61126708+frogg-kek@users.noreply.github.com>
2024-11-21 23:34:05 +0200

    Update login.php

--- pagrindinis/login.php
+++ pagrindinis/login.php
@@ -52,7 +52,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Form</title>
-    <link rel="stylesheet" href="logdesing.css">
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
     <div class="overlay">
